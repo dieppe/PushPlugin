2830ab74ad67714be7874c1f430842977d14f2e7
Kristina Koeva <kristinalkoeva@gmail.com>
2013-09-30 09:55:11 +0300

    Make plugin Icenium compatible. Port to Cordova 3.0

--- www/PushNotification.js
+++ www/PushNotification.js
@@ -1,13 +1,14 @@
-
-var PushNotification = function() {
+var PushNotification = function () {
 };
 
-
 // Call this to register for push notifications. Content of [options] depends on whether we are working with APNS (iOS) or GCM (Android)
-PushNotification.prototype.register = function(successCallback, errorCallback, options) {
-    if (errorCallback == null) { errorCallback = function() {}}
+PushNotification.prototype.register = function (successCallback, errorCallback, options) {
+    if (errorCallback == null) {
+        errorCallback = function () {
+        }
+    }
 
-    if (typeof errorCallback != "function")  {
+    if (typeof errorCallback != "function") {
         console.log("PushNotification.register failure: failure parameter not a function");
         return
     }
@@ -17,14 +18,17 @@ PushNotification.prototype.register = function(successCallback, errorCallback, o
         return
     }
 
-	cordova.exec(successCallback, errorCallback, "PushPlugin", "register", [options]);
+    cordova.exec(successCallback, errorCallback, "PushPlugin", "register", [options]);
 };
 
 // Call this to unregister for push notifications
-PushNotification.prototype.unregister = function(successCallback, errorCallback) {
-    if (errorCallback == null) { errorCallback = function() {}}
+PushNotification.prototype.unregister = function (successCallback, errorCallback) {
+    if (errorCallback == null) {
+        errorCallback = function () {
+        }
+    }
 
-    if (typeof errorCallback != "function")  {
+    if (typeof errorCallback != "function") {
         console.log("PushNotification.unregister failure: failure parameter not a function");
         return
     }
@@ -34,15 +38,17 @@ PushNotification.prototype.unregister = function(successCallback, errorCallback)
         return
     }
 
-     cordova.exec(successCallback, errorCallback, "PushPlugin", "unregister", []);
+    cordova.exec(successCallback, errorCallback, "PushPlugin", "unregister", []);
 };
- 
- 
-// Call this to set the application icon badge
-PushNotification.prototype.setApplicationIconBadgeNumber = function(successCallback, errorCallback, badge) {
-    if (errorCallback == null) { errorCallback = function() {}}
 
-    if (typeof errorCallback != "function")  {
+// Call this to set the application icon badge
+PushNotification.prototype.setApplicationIconBadgeNumber = function (successCallback, errorCallback, badge) {
+    if (errorCallback == null) {
+        errorCallback = function () {
+        }
+    }
+
+    if (typeof errorCallback != "function") {
         console.log("PushNotification.setApplicationIconBadgeNumber failure: failure parameter not a function");
         return
     }
@@ -52,14 +58,11 @@ PushNotification.prototype.setApplicationIconBadgeNumber = function(successCallb
         return
     }
 
-    cordova.exec(successCallback, successCallback, "PushPlugin", "setApplicationIconBadgeNumber", [{badge: badge}]);
+    cordova.exec(successCallback, successCallback, "PushPlugin", "setApplicationIconBadgeNumber", [{ badge: badge }]);
 };
 
 //-------------------------------------------------------------------
 
-if(!window.plugins) {
-    window.plugins = {};
-}
-if (!window.plugins.pushNotification) {
-    window.plugins.pushNotification = new PushNotification();
-}
+var pushNotification = new PushNotification();
+
+module.exports = pushNotification;
